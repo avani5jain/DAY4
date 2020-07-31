bc7465e2c9504d42636ad47207354e37702963e3
avani5jain <avani.jain.cs.2016@miet.ac.in>
2020-07-31 18:39:18 +0530

    Box Generator

--- box.js
+++ box.js
@@ -2,11 +2,15 @@ var c = 0;
 
 function bigger() {
     var a = document.getElementById('number1').value;
-    for (let i = 0; i < Number; i++) {
+    var color = document.getElementById('clr').value;
+    a = Number(a);
+
+    for (let i = 0; i < a; i++) {
         c++;
         var box = document.createElement("button");
         box.id = 'box1';
         box.innerHTML = "new button" + c;
+        box.style = "background-color:" + (color) + ";"
         document.body.appendChild(box);
     }
 
